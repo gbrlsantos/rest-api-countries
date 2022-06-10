899d7b05293d84eef35a79b7b30b77928577ed07
gcruzdev <gabrielasnts30@gmail.com>
2022-06-10 00:45:58 -0300

    context provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react'
 import Pages from './Components/Pages'
 
 import SearchProvider from './Context/Search'
+import RegionFilterProvider from './Context/RegionFilter';
 
 function App() {
   return (
-    <SearchProvider>
-      <div className="App">
-        <Pages />
-      </div>
-    </SearchProvider>
+    <RegionFilterProvider>
+      <SearchProvider>
+        <div className="App">
+          <Pages />
+        </div>
+      </SearchProvider>
+    </RegionFilterProvider>
   );
 }
 
